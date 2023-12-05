a7849ae91d055fe9fcb0842c1a0cd7838e701369
Rafael Francisco <rafaeldeoleo14@gmail.com>
2023-12-05 15:10:01 -0400

    feat: added railway mysql db

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,9 +1,9 @@
 
 export const PORT = process.env.PORT || 3000
 
-export const DB_HOST = process.env.DB_HOST || 'localhost'
-export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || '23867459'
-export const DB_NAME = process.env.DB_NAME || 'card_test'
-export const DB_PORT = process.env.DB_PORT || 3306 
+export const DB_HOST = 'roundhouse.proxy.rlwy.net' || 'localhost'
+export const DB_USER = 'root'
+export const DB_PASSWORD = 'gC3CfgcD1d6-Cae6G4H4g15DbDHH1GGD' || '23867459'
+export const DB_NAME = 'railway' || 'card_test'
+export const DB_PORT = 48781 || 3306 
 
