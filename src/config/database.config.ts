493254ed56e5db68256273d1ebb58979d348f39b
Rafael Francisco <rafaeldeoleo14@gmail.com>
2023-12-05 14:30:07 -0400

    feat: added config for build

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,9 +1,11 @@
-import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize('card_test', 'root', '23867459', {
-    host: 'localhost',
+import { Sequelize } from "sequelize";
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from "./env.config";
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: DB_HOST,
     dialect: 'mysql',
-    port: 3306
+    port: Number(DB_PORT) || undefined
 });
 
 const testConnection = async ()=> {
